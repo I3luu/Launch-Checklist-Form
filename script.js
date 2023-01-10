d636baecbff64ecca79a9488888b8f50fb0d709d
Brandon <brobinson3030@gmail.com>
2023-01-10 10:47:12 -0500

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Write your JavaScript code here!
-
+window.addEventListener("load", function(){
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+   
+   });
+});
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ul>
@@ -11,3 +17,8 @@
 </ul>
 <img src="${}">
 */
+
+// TODO 1: set up an window load handler
+// TODO 2: set up a submit handler for the form
+// TODO 3: cancel submission using event.preventDefault
+// TODO 4: validate all inputs have data in them
